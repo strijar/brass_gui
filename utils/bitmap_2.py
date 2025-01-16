d4a00a990f3927ad6cc357887465d16e33af3a6f
Belousov Oleg <belousov.oleg@gmail.com>
2025-01-16 20:06:07 +0300

    Update bitmaps

--- utils/bitmap_2.py
+++ utils/bitmap_2.py
@@ -3,6 +3,8 @@ import cairo
 def bitmap(width, height, name, shadow = 5.0):
 	corner = 10
 	lines = 3
+	embose = 3
+	embose_lines = 1.5
 
 	surface = cairo.ImageSurface(cairo.FORMAT_ARGB32, width, height)
 	ctx = cairo.Context(surface)
@@ -48,11 +50,23 @@ def bitmap(width, height, name, shadow = 5.0):
 	ctx.set_line_width(lines)
 	ctx.set_source_rgba(1.0, 1.0, 1.0, 0.5)
 
-	ctx.move_to(border, border + height)
-	ctx.rel_line_to(0, -height + corner)
+	ctx.move_to(border, border + height - embose)
+	ctx.rel_line_to(0, -height + corner + embose)
 	ctx.rel_line_to(corner, -corner)
-	ctx.rel_line_to(width - corner, 0)
+	ctx.rel_line_to(width - corner - embose, 0)
 	ctx.stroke()
+
+	if embose > 0:
+		ctx.set_line_width(embose_lines)
+		ctx.set_source_rgba(1.0, 1.0, 1.0, 0.25)
+
+		ctx.move_to(border + width - embose, border + embose)
+		ctx.rel_line_to(0, height - corner - embose - embose/2)
+		ctx.rel_line_to(-corner + embose/2, corner - embose/2)
+		ctx.rel_line_to(-width + corner + embose * 2, 0)
+		ctx.stroke()
+
+    #
 
 	ctx.set_source_rgba(0, 0, 0, 0.5)
 
@@ -61,5 +75,15 @@ def bitmap(width, height, name, shadow = 5.0):
 	ctx.rel_line_to(-corner, corner)
 	ctx.rel_line_to(-width + corner, 0)
 	ctx.stroke()
+
+	if embose > 0:
+		ctx.set_line_width(embose_lines)
+		ctx.set_source_rgba(0, 0, 0, 0.5)
+
+		ctx.move_to(border + embose, border + height - embose)
+		ctx.rel_line_to(0, -height + corner + embose + embose/2)
+		ctx.rel_line_to(corner - embose/2, -corner + embose/2)
+		ctx.rel_line_to(width - corner - embose * 2, 0)
+		ctx.stroke()
 
 	surface.write_to_png(name)
